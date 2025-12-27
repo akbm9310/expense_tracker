e7b9c4a793b8e36277464cb71e911b7d6839f0e6
Akash <akbm95326@gmail.com>
2025-12-27 17:42:02 +0530

    Connect Frontend to Render

--- expenseTracker/src/main.jsx
+++ expenseTracker/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
+axios.defaults.baseURL = "https://expense-tracker-api-vge5.onrender.com";
 axios.defaults.withCredentials = true;
 import "./index.css";
 import App from "./App.jsx";
